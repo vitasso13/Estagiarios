355d96ecb4250dd9a8a39542541e27ef76b56cda
vitasso13 <43220494+vitasso13@users.noreply.github.com>
2020-11-10 16:05:07 -0300

    PostgresSQL implementado

---
+++ backend/src/main/java/com/estagio/backend/dao/FakeEstagiarioDataAccessService.java
@@ -0,0 +1,73 @@
+package com.estagio.backend;
+
+import com.estagio.backend.dao.EstagiarioDao;
+import com.estagio.backend.model.Estagiario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.List;
+@Repository("postgres")
+public class FakeEstagiarioDataAccessService implements EstagiarioDao {
+    private static final List<Estagiario> DB = new ArrayList<>();
+
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public FakeEstagiarioDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public int insertEstagiario(UUID id, Estagiario estagiario) {
+        DB.add(new Estagiario(id, estagiario.getNome(), estagiario.getEmail(), estagiario.getTelefone(), estagiario.getFoto(), estagiario.getComprovanteMatricula(), estagiario.getInteresses(), estagiario.getDominios()));
+        return 1;
+    }
+
+    @Override
+    public List<Estagiario> selectAllPeople() {
+
+
+        return DB;
+    }
+
+    @Override
+    public Optional<Estagiario> selectEstagiarioById(UUID id) {
+        return DB.stream().filter(estagiario -> estagiario.getId().equals(id)).
+                findFirst();
+    }
+
+    @Override
+    public int deleteEstagiarioById(UUID id) {
+        Optional<Estagiario> estagiarioMaybe = selectEstagiarioById(id);
+        if(estagiarioMaybe.isEmpty()){
+            return 0;
+        }
+
+        DB.remove(estagiarioMaybe.get());
+        return 1;
+    }
+
+    @Override
+    public int updateEstagiarioById(UUID id, Estagiario update) {
+
+        return selectEstagiarioById(id)
+                .map(estagiario ->{
+                    int indexOfEstagiarioToUpdate = DB.indexOf(estagiario);
+                    if(indexOfEstagiarioToUpdate >= 0) {
+                        DB.set(indexOfEstagiarioToUpdate, new Estagiario(id, update.getNome(),
+                                update.getEmail(),
+                                update.getTelefone(),
+                                update.getFoto(),
+                                update.getComprovanteMatricula(),
+                                update.getInteresses(),
+                                update.getDominios()));
+                        return 1;
+                    }
+                    return 0;
+                })
+                .orElse(0);
+    }
+}
---
+++ backend/src/main/java/com/estagio/backend/datasource/PostgresDatasource.java
@@ -0,0 +1,2 @@
+package com.estagio.backend.datasource;public class PostgresDatasource {
+}
